3daa4683f548dca48730f9d44a314cc6a1e6e122
tymsk3891 <tymsk3891@outlook.com>
2018-05-25 01:30:37 +0900

    error handling sample
    example:
    exec 1>
    exec 2>&1

---
+++ exec_log.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+log_file="`basename $0 .sh`.log"
+err_file="`basename $0 .sh`.err"
+
+exec 1> $log_file
+exec 2> $err_file
+
+
+echo "LOG"
+echo 
+echo "ERROR" >&2
+echo 
+
+exit 0
